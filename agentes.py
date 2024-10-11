9246254da7e6166295ca2f066546805088f485db
C4R10SHenrique <car4los007@gmail.com>
2024-10-11 16:06:32 -0500

    Estrutura de controle/condici

---
+++ agentes.py
@@ -0,0 +1,16 @@
+name = input("Digite o nome do seu agente (Jett,Omen,Sage,Gekko): ").strip().lower()
+#classificando agentes 
+if name == "jett":
+    print ("Seu personagem é um duelista")
+elif name == "omen":
+    print ("Seu personagem é um controlador")
+elif name == "sage":
+    print ("Seu personagem é um sentinela")
+elif name == "gekko":
+    print ("Seu personagem é um iniciador")           
+else:
+    print ("Agente não reconhecido")
+
+
+
+
